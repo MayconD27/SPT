22b12244fd6310401809466d734fea0b11ba3505
Thiago11v <thiagocry.v.a@gmail.com>
2023-09-28 21:11:59 -0300

    tela de erro

--- admin_tela/inseriruser.php
+++ admin_tela/inseriruser.php
@@ -1,10 +1,7 @@
 <?php
 $nome_User = $_POST["nome"];
 $cpf_User = $_POST["cpf"];
-$email_User = $_POST["email"]
-$telefone_User = $_POST["telefone"]
-$qtd_User = $_POST["qtd_dias"]
-
-
-
+$email_User = $_POST["email"];
+$telefone_User = $_POST["telefone"];
+$qtd_User = $_POST["qtd_dias"];
 ?>
---
+++ mensagem_erro.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mensagem de Erro</title>
+    <link rel="stylesheet" href="SPT/css/estilo_erro.css">
+</head>
+<body>
+    <div>
+        <img src="img/sinal-de-aviso.png" alt="" class="erro" style="border:none">
+        <h1>Falha no login</h1>
+        <p>Tente novamente</p>
+        <button onclick="window.history.back();">Voltar</button>
+    </div>
+</body>''
+</html>
